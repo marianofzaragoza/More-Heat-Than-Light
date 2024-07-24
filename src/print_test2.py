6ae8d575b3036a22c47c7688962480ae3428a814
Raspberry5_num1 <agustina@AlicePrinter>
2024-07-24 23:50:34 +0200

    Some modifications for temperature continuos sensing

--- src/print_test2.py
+++ src/print_test2.py
@@ -1,14 +1,13 @@
 import StarTSPImage
 from PIL import Image, ImageDraw
 
-image = Image.new('RGB', (500, 500), color='White')
-draw = ImageDraw.Draw(image)
 
-something = 480
-while something > 300:
-    draw.ellipse((490 - something, 490 - something, something, something), fill='White')
-    draw.ellipse((500 - something, 500- something, something - 10, something -10), fill='Red')
-    something = something - 20
+image = Image.new('RGB', (500, 10000), color='White')
+
+draw = ImageDraw.Draw(image)
+draw.line((0, 0, 500, 10000), fill='Black', width=3)
+
+
 
 raster = StarTSPImage.imageToRaster(image, cut=False)
 
