52a20bac2698a9f0cdaa240e6c324bc7eb6ea98c
Your Name <you@example.com>
2024-08-06 15:52:13 +0200

    mul

--- testmulti.sh
+++ testmulti.sh
@@ -2,6 +2,7 @@
 #
 sudo sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=0
 sudo ip addr add 224.10.10.10/24 dev eth0 autojoin
+sudo ip addr add 239.192.1.100/24 dev eth0 autojoin
 sudo ip -f inet maddr show dev eth0
 sudo ping -I eth0 224.10.10.10
 
