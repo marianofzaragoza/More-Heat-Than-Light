d69205aecb52f20c610f46fa74caeef4937a375c
Your Name <you@example.com>
2024-08-06 16:08:05 +0200

    mu

--- testmulti.sh
+++ testmulti.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 #
 sudo sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=0
+sudo sysctl -w net.ipv6.icmp_echo_ignore_broadcasts=0
+
+
+sudo sysctl -w net.ipv4.conf.all.force_igmp_version=2
+
+
 sudo ip addr add 224.10.10.10/24 dev eth0 autojoin
 sudo ip addr add 239.192.1.100/24 dev eth0 autojoin
 sudo ip -f inet maddr show dev eth0
