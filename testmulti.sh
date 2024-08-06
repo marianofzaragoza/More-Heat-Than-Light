b5f99cc62f8578812143ff7629218723b576e29d
bob <bob@moreheat>
2024-08-06 15:37:34 +0200

    multi

---
+++ testmulti.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+sudo sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=0
+sudo ip addr add 224.10.10.10/24 dev eth0 autojoin
+sudo ip -f inet maddr show dev eth0
+sudo ping -I eth0 224.10.10.10
+
+
+
