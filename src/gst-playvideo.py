d6d776a8ec2fb3caeae7c358115b3c9712c48926
Your Name <you@example.com>
2024-08-07 17:59:55 +0200

    cairo

--- src/gst-playvideo.py
+++ src/gst-playvideo.py
@@ -65,7 +65,7 @@ class Player(Gtk.Window):
         context.select_font_face('Open Sans', cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
         context.set_font_size(40)
         context.move_to(100, 100)
-        context.text_path('HELLO')
+        context.text_path('HELLO' + str(_timestamp) + ' ' + str(_duration))
         context.set_source_rgb(0.5, 0.5, 1)
         context.fill_preserve()
         context.set_source_rgb(0, 0, 0)
@@ -97,11 +97,17 @@ class Player(Gtk.Window):
  #       overlaysink = Gst.parse_bin_from_description("cairooverlay name=coverlay ! queue ! glimagesink", 'overlaysink')
 
         cairo = False
+        comp = True
         if cairo:
             print("USING CAIRO OVERLAY")
             overlaysink = Gst.parse_bin_from_description("cairooverlay name=coverlay ! queue ! glimagesink", 'overlaysink')
             cairo_overlay = overlaysink.get_by_name('coverlay')
             cairo_overlay.connect('draw', self.on_draw)
+        elif comp:
+            overlaysink = Gst.parse_bin_from_description("overlaycomposition name=coverlay ! queue ! glimagesink", 'overlaysink')
+            cairo_overlay = overlaysink.get_by_name('coverlay')
+            cairo_overlay.connect('draw', self.on_composition_draw)
+
         else: 
             overlaysink = Gst.parse_bin_from_description("videoconvert ! queue ! timeoverlay ! queue ! videoconvert ! queue ! glimagesink", 'overlaysink')
 
@@ -156,8 +162,30 @@ class Player(Gtk.Window):
         bus.enable_sync_message_emission()
         bus.connect('sync-message::element', self.on_sync_message)
 
+    '''
+    // Connect to the overlaycomposition element's "draw" signal, which is emitted for
+    // each videoframe piped through the element. The signal handler needs to
+    // return a gst_video::VideoOverlayComposition to be drawn on the frame
+    //
+    // Signals connected with the connect(<name>, ...) API get their arguments
+    // passed as array of glib::Value. For a documentation about the actual arguments
+    // it is always a good idea to check the element's signals using either
+    // gst-inspect, or the online documentation.
+    //
+    // In this case, the signal passes the gst::Element and a gst::Sample with
+    // the current buffer
+    '''
+    #https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/blob/main/examples/src/bin/overlay-composition.rs
+    def  on_composition_draw(element, sample, userdata):
+        print(element)
+        print(sample)
+        print(userdata)
 
- 
+        #<__main__.Player object at 0x7f84a2990a00 (__main__+Player at 0x1ddc270)>
+        #<__gi__.GstOverlayComposition object at 0x7f84a31ec740 (GstOverlayComposition at 0x22ce340)>
+        #<Gst.Sample object at 0x7f84967b3dd0 (GstSample at 0x7f84440b72d0)>
+
+
     def __init__(self):
         GObject.threads_init()
         self.init_gui()
