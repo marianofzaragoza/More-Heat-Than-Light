f61fceb5a003091053e3e1f85d23409c9d011033
Your Name <you@example.com>
2024-08-28 15:47:27 +0200

    testpip

--- testpip.sh
+++ testpip.sh
@@ -1,19 +1,43 @@
-
-export GST_DEBUG_DUMP_DOT_DIR=./debug/ 
-export GST_DEBUG=4 
+#export GST_DEBUG_DUMP_DOT_DIR=./debug/ 
+#export GST_DEBUG=4 
 export DISPLAY=:0 
 #gst-launch-1.0 uridecodebin uri=file:///home/user/src/More-Heat-Than-Light/videos/entanglement.mp4 ! videoconvert ! kmssink
 
 
 DIR=`pwd`
 #SRC="$DIR/testfile/quality/4k-h265.mp4"
-SRC="$DIR/testfile/quality/movie1.mp4"
-SRC2="$DIR/testfile/quality/movie2.mp4"
-
-
-
+SRC="$DIR/video/quality/movie1.mp4"
+SRC2="$DIR/video/animation/alice_hd.mov" 
+#SRC2="$DIR/video/animation/Alice_3840x2160_v1.mov"
+#SRC2="$DIR/video/quality/movie2.mp4"
+SRC="$DIR/video/quality/HD PRORESS.mov"
 #SRC2="$DIR/testfile/quality/4k-prores.mov"
 #SRC2="$DIR/testfile/bbb_sunflower_2160p_30fps_stereo_abl.mp4"
+
+lala () {
+gst-launch-1.0 \
+    uridecodebin3 uri="file://${SRC2}" !\
+    videoconvert ! videorate ! glupload ! glcolorconvert ! glimagesink\
+
+    exit 0
+gst-launch-1.0 \
+    uridecodebin3 uri="file://${SRC2}" name=demux2 ! \
+    queue2 ! queue ! videorate ! queue ! glupload ! glcolorconvert ! glcolorscale ! \
+    glimagesink
+
+exit 0
+}
+
+#! video/x-raw-yuv,width=1280,height=720,framerate=30/1 !
+gst-launch-1.0 \
+    glvideomixer name=videomix latency=1000 sink_1::alpha=0.5 message-forward=false start-time-selection=2  ! glimagesink sync=false\
+    uridecodebin uri="file://${SRC}" name=demux1 ! queue leaky=0 ! videoconvert ! videorate ! queue ! glupload !glcolorconvert ! glcolorscale ! videomix. \
+    uridecodebin uri="file://${SRC2}" name=demux2 ! \
+    queue leaky=0 ! decodebin ! queue  ! videoconvert ! queue ! videorate ! glupload ! glcolorconvert  ! glcolorscale ! videomix.
+# ! video/x-raw,width=640,height=360!
+
+
+exit 0
 
 
 gst-launch-1.0 \
@@ -22,6 +46,8 @@ gst-launch-1.0 \
     uridecodebin uri=file://$SRC2 name=demux2 ! \
     queue2 ! decodebin ! queue  ! videorate ! glupload ! glcolorconvert  ! glcolorscale ! videomix.
 # ! video/x-raw,width=640,height=360!
+
+
 exit 0
 queue2 ! audioconvert ! audioresample ! audiomix. \
     audiomixer name=audiomix !  audioconvert ! autoaudiosink sync=false \
---
+++ video/animation/convert.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#  Stream #0:0[0x1](eng): Video: prores (4444) (ap4h / 0x68347061), yuva444p12le(tv, bt709, progressive), 3840x2160, 365922 kb/s, SAR 1:1 DAR 16:9, 24 fps, 24 tbr, 24 tbn (default)
+
+ffmpeg -y -i Alice_3840x2160_v1.mov \
+  -codec prores_ks \
+  -pix_fmt yuva444p12le \
+  -alpha_bits 16 \
+  -profile:v 4444 \
+  -s 1920x1080 \
+  -f mov \
+  alice_hd.mov
+
+exit 1
+ffmpeg -y -i filename.mov \
+  -r 29.97 -codec prores_ks \
+  -pix_fmt yuva444p10le \
+  -alpha_bits 16 \
+  -profile:v 4444 \
+  -f mov \
+  filenameb.mov
