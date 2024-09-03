ec3ca03a18f20203cc73f32866860443b030efa2
Your Name <you@example.com>
2024-09-04 01:51:24 +0200

    bootscript

--- run-boot.sh
+++ run-boot.sh
@@ -6,4 +6,32 @@ cd /home/user/src/More-Heat-Than-Light/
 #./run.sh src/gstinter_overlay_experiment.py
 
 
-./run.sh src/gst-player.py
+
+case `hostname` in
+
+  debian)
+    ./run.sh src/gst-player.py
+    ;;
+  alice-vid)
+    ./run.sh src/gst-player.py
+    ;;
+  bob-vid)
+    ./run.sh src/gst-player.py
+    ;;
+  alice)
+    ./run.sh src/tempsender.py
+    ;;
+
+  bob)
+    ./run.sh src/tempsender.py
+    ;;
+  carol)
+    ./run.sh src/printer.py
+    ;;
+
+  *)
+    echo -n "unknown"
+    ./run.sh src/gst-player.py
+
+    ;;
+esac
