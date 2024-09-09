8bfd05c94ce83bd4748d754ac78fc523be7abcc4
user <user@test-vid>
2024-09-09 19:47:03 +0200

    dw

--- src/gst-player.py
+++ src/gst-player.py
@@ -338,7 +338,8 @@ class PlayerUi(Gtk.Window):
                 if ( value == 127 and rxtime < entseconds + 2 and rxtime > entseconds - 3 )  and not self.player.in_entanglement and self.player.pre_entanglement:
                     print('ENTANG, plusminus time match')
  
-                if ( value == 127 and rxtime < entseconds + 2 and rxtime > entseconds - 2 )  and not self.player.in_entanglement and self.player.pre_entanglement:
+                #if ( value == 127 and rxtime < entseconds + 2 and rxtime > entseconds - 2 )  and not self.player.in_entanglement and self.player.pre_entanglement:
+                if (value == 127 and rxtime == entseconds)  and not self.player.in_entanglement and self.player.pre_entanglement:
                     print("ENTANGLEMENT")
                     self.player.playlist.send_specific_midi(19) 
                     #self.tempsender.send_temp(cancel_entanglement=True)
