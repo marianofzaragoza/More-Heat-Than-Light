59478962b5b53d019c18707b5918535702f08ce7
user <user@alice-vid>
2024-09-12 17:53:02 +0200

    boot delay

--- installrtpmidi.sh
+++ installrtpmidi.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #
-sudo apt install -y libfmt9 avahi-daemon
+sudo apt install -y libfmt9 avahi-daemon alsa-utils
 sudo dpkg -i librtpmidid0_23.12_amd64.deb
 sudo dpkg -i rtpmidid_23.12_amd64.deb
